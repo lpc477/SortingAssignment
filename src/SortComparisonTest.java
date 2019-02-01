c6f16d546af62f8f6a178a5f0a729969347e5a16
Unknown <lpc477@gmail.com>
2019-02-01 14:50:55 +0000

    Variable Declaration in testEmpty()
    Just the beginning of the test empty function

--- src/SortComparisonTest.java
+++ src/SortComparisonTest.java
@@ -30,13 +30,16 @@ public class SortComparisonTest
     @Test
     public void testEmpty()
     {
+    	SortComparison testSort = new SortComparison();
+    	double a[];
     }
 
 
     // TODO: add more tests here. Each line of code and ech decision in Collinear.java should
     // be executed at least once from at least one test.
 
-    // ----------------------------------------------------------
+
+	// ----------------------------------------------------------
     /**
      *  Main Method.
      *  Use this main method to create the experiments needed to answer the experimental performance questions of this assignment.
